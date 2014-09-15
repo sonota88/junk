ade3dded82a343a20e31615e35b26b80afac2f24
sonota <yosiot8753@gmail.com>
2014-09-16 05:17:15 +0900

    add execSync

--- jrunscript_rhino/my_init.js
+++ jrunscript_rhino/my_init.js
@@ -109,4 +109,159 @@
 
     return _File;
   })();
+
+
+  ////////////////////////////////
+
+
+  (function(){
+    function createArray(type, size){
+      var _type = type;
+      if(type === "Byte"){
+        _type = java.lang.Byte.TYPE;
+      }
+      return java.lang.reflect.Array.newInstance(
+        _type, size);
+    };
+
+    // ----------------
+
+    var ByteArrayUtil = (function(){
+
+      var ByteArrayUtil = {};
+
+      ByteArrayUtil.substr = function(array, from, to){
+        var size;
+        if(to){
+          size = to - from;
+        }else{
+          size = array.length - from;
+        }
+        var result = createArray("Byte", size);
+          for(var i=0; i<size; i++){
+            result[i] = array[from + i];
+          }
+        return result;
+      };
+
+      ByteArrayUtil.toStr = function(bary, encoding){
+        if(!encoding){
+          encoding = "UTF-8";
+        }
+        return new java.lang.String(bary, encoding);
+      };
+
+      return ByteArrayUtil;
+    })();
+
+    // ----------------
+
+    function makeReaderThread(is, out, enc){
+      var _enc = enc || "UTF-8";
+      return new java.lang.Thread(function(){
+        var n;
+        var bufSize = 1024;
+        var buf = createArray("Byte", bufSize);
+        while(true){
+          n = is.read(buf, 0, bufSize);
+          if(n <= 0){
+            break;
+          }
+          out.print(ByteArrayUtil.toStr(
+            ByteArrayUtil.substr(buf, 0, n), _enc));
+        }
+      });
+    }
+
+    /**
+     * @param opts
+     *   outPrinter: default: System.out
+     *   errPrinter: default: System.err
+     */
+    var _execSync = function(args, workDir, opts){
+      if(typeof args === "string"){
+        args = args.split( /\s+/ );
+      }
+      workDir = workDir || ".";
+      opts = opts || {};
+
+      // println("exec command: " + args.join(" "));
+      var javaArgs = createArray(
+        java.lang.String, args.length);
+      args.forEach(function(e, i){
+        javaArgs[i] = e;
+      });
+
+      var pb = new java.lang.ProcessBuilder(javaArgs);
+      pb.directory(new File(workDir));
+
+      var proc = pb.start();
+      var is = proc.getInputStream();
+      var es = proc.getErrorStream();
+
+      var outPrinter = opts.outPrinter || java.lang.System.out;
+      var errPrinter = opts.errPrinter || java.lang.System.err;
+
+      var isTh = makeReaderThread(is, outPrinter, opts.encoding);
+      var esTh = makeReaderThread(es, errPrinter, opts.encoding);
+
+      isTh.start();
+      esTh.start();
+
+      while(isTh.isAlive() || isTh.isAlive()){
+        java.lang.Thread.sleep(100);
+      }
+
+      var status = proc.waitFor();
+
+      if(status !== 0){
+        throw new Error("exec failed " + status);
+      }
+
+      return status;
+    };
+
+    /**
+     * out, err はstdout/stderrに出すだけ。
+     */
+    global.execSync = function(args, workDir){
+      var status = _execSync(args, workDir);
+
+      return {
+        status: status
+      };
+    };
+    global.system = global.execSync;
+
+    /**
+     * out, err を文字列として取得する。
+     */
+    global.execSyncStr = function(args, workDir, enc){
+      var swout = new StringWriter();
+      var swerr = new StringWriter();
+      var _enc = enc || "UTF-8";
+      var status;
+
+      try{
+        status = _execSync(
+          args, workDir
+         ,{ outPrinter: new PrintWriter(swout)
+           ,errPrinter: new PrintWriter(swerr)
+           ,encoding: _enc
+          }
+        );
+      }catch(e){
+        println(e);
+        println("[OUT]\n" + swout.toString());
+        println("[ERR]\n" + swerr.toString());
+      }
+
+      return {
+        status: status
+       ,out: "" + swout.toString()
+       ,err: "" + swerr.toString()
+      };
+    };
+
+  })();
 })();
