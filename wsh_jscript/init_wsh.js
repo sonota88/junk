2d5d66df63652bd0d4571092e498433ff45ea4f5
sonota <yosiot8753@gmail.com>
2014-09-27 21:30:01 +0900

    add feature: require

--- wsh_jscript/init_wsh.js
+++ wsh_jscript/init_wsh.js
@@ -29,7 +29,25 @@ function puts(){
 var _File = (
     function(){
 
-        var _File = {};
+        var _File = {
+          join: function(){
+            var path = "";
+            each(arguments, function(arg){
+              if( path === "" || path.match( /\/$/ )){
+                ;
+              }else{
+                path += "/";
+              }
+              path += arg;
+            });
+            return path;
+          }
+
+          ,exists: function(path){
+            var fso = new ActiveXObject("Scripting.FileSystemObject");
+            return fso.FileExists(path);
+          }
+        };
 
         _File.read = function(path, charset){
             var sin = new ActiveXObject("ADODB.Stream");
@@ -52,9 +70,61 @@ var _File = (
             sout.saveToFile(path, 2);
             sout.close();
         };
-        
+
         return _File;
     })();
+
+function _load(str){
+  var src = _File.read(str);
+  var scriptFullPath = str;
+  try {
+    eval(
+      '(function(){ var __FILE__ = "' + scriptFullPath + '"; '
+        + src
+        + " })();"
+    );
+  } catch(e) {
+    // dumpError(e);
+    throw e;
+  } finally {
+    ;
+  }
+}
+
+function require(path){
+  global.exports = global.exports || {};
+  var exports = global.exports;
+
+  if( ! path.match( /\.js$/) ){
+    path = path + ".js";
+  }
+
+  var foundPath;
+  each(global.LOAD_PATH, function(loadPath){
+    var fullPath;
+    if(path.match( /^\// )){
+      fullPath = path;
+    }else{
+      fullPath = _File.join(loadPath, path);
+    }
+    if(_File.exists(fullPath)){
+      foundPath = fullPath;
+      return false; // break
+    }
+  });
+
+  if(foundPath){
+    _load(foundPath);
+  }else{
+    throw new Error("module not found: " + path);
+  }
+  var obj;
+  for(var k in exports){
+    obj = exports[k];
+  }
+  exports = {};
+  return obj;
+}
 
 // --------------------------------
 
@@ -62,3 +132,4 @@ global.each = each;
 global.print = print;
 global.puts = puts;
 global._File = _File;
+global.require = require;
--- wsh_jscript/sample.js
+++ wsh_jscript/sample.js
@@ -1,6 +1,24 @@
 // sjis/crlf
 
+(function(){
+  var global = this;
+  var stdLibDir = "C:/path/to/wsh_jscript";
+  global.__FILE__ = WScript.ScriptFullName.replace( /\\/g, "/");
+  global.LOAD_PATH = [
+    global.__FILE__.replace( /^(.*)\/.+?$/, '$1')
+    , stdLibDir
+  ];
+
+  function _fileExists(path){
+    var fso = new ActiveXObject("Scripting.FileSystemObject");
+    return fso.FileExists(path);
+  }
+
 function _readFileSjis(path){
+    if( ! _fileExists(path)){
+        throw new Error("file not found: " + path);
+    }
+
     var sin = new ActiveXObject("ADODB.Stream");
     sin.type = 2; // text
     sin.charset = "Shift_JIS";
@@ -16,9 +34,12 @@ function load(path){
     eval("(function(){ " + src + " })();");
 }
 
+  load(stdLibDir + "/init_wsh.js");
+  load(stdLibDir + "/my_init.js");
+})();
+
 // --------------------------------
 
-load("init_wsh.js");
 print(1);
 puts(2);
 
@@ -28,3 +49,6 @@ each([11, 12, 13], function(it){
 
 _File.write("temp.txt", "‚  FDSA " + new Date());
 puts(_File.read("temp.txt", "Shift_JIS"));
+
+// var calc = require("calc");
+// puts(calc.plus(1, 2));
