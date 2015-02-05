f04594c889e47e582a97a5c26004704748145996
sonota88 <yosiot8753@gmail.com>
2015-02-05 23:39:03 +0900

    move writeStringResponse to HttpExchangeWrapper

--- jrunscript_rhino/lib/http_exchange_wrapper.js
+++ jrunscript_rhino/lib/http_exchange_wrapper.js
@@ -2,6 +2,35 @@ var HttpExchangeWrapper = (function(){
   
   "use strict";
 
+  /**
+   * @param {string} jsString
+   * @return {java.lang.String}
+   */
+  function toJavaString(jsString){
+    return new java.lang.String(jsString);
+  };
+
+  function withOutputStreamWriter(fos, encoding, fn){
+    var osw;
+    try {
+      osw = new OutputStreamWriter(fos, encoding);
+      fn(osw);
+    } finally {
+      if(osw){ osw.close(); }
+    }
+  }
+
+  function withBufferedWriter(writer, fn){
+    var bw;
+    try {
+      bw = new BufferedWriter(writer);
+      fn(writer);
+    } finally {
+      if(bw){ bw.close(); }
+    }
+  }
+
+
   function HttpExchangeWrapper(he){
     this._he = he;
   }
@@ -16,6 +45,23 @@ var HttpExchangeWrapper = (function(){
     for(var key in obj){
       headers.add(key, obj[key]);
     }
+  };
+
+  __.writeStringResponse = function(status, body){
+    var javaStr = new toJavaString(body);
+
+    this._he.sendResponseHeaders(
+      status, javaStr.getBytes("UTF-8").length);
+
+    var os = this._he.getResponseBody();
+
+    withOutputStreamWriter(os, "UTF-8", function(w){
+      withBufferedWriter(w, function(bw){
+        bw.write(javaStr);
+      });
+    });
+
+    os.close();
   };
 
   return HttpExchangeWrapper;
--- jrunscript_rhino/lib/http_utils.js
+++ jrunscript_rhino/lib/http_utils.js
@@ -1,33 +1,5 @@
 var HttpUtils = (function(){
 
-  /**
-   * @param {string} jsString
-   * @return {java.lang.String}
-   */
-  function toJavaString(jsString){
-    return new java.lang.String(jsString);
-  };
-
-  function withOutputStreamWriter(fos, encoding, fn){
-    var osw;
-    try {
-      osw = new OutputStreamWriter(fos, encoding);
-      fn(osw);
-    } finally {
-      if(osw){ osw.close(); }
-    }
-  }
-
-  function withBufferedWriter(writer, fn){
-    var bw;
-    try {
-      bw = new BufferedWriter(writer);
-      fn(writer);
-    } finally {
-      if(bw){ bw.close(); }
-    }
-  }
-
   function escapeHTML(src){
     return src
     .replace(/&/g, "&amp;")
@@ -39,24 +11,6 @@ var HttpUtils = (function(){
   ////////////////////////////////
 
   function HttpUtils(){}
-
-  HttpUtils.writeStringResponse = function(he, status, str){
-
-    var javaStr = new toJavaString(str);
-
-    he.sendResponseHeaders(
-      status, javaStr.getBytes("UTF-8").length);
-
-    var os = he.getResponseBody();
-
-    withOutputStreamWriter(os, "UTF-8", function(w){
-      withBufferedWriter(w, function(bw){
-        bw.write(javaStr);
-      });
-    });
-
-    os.close();
-  };
 
   HttpUtils.makeErrorPageHtmlForDebug = function(ex){
     var html = '<html><body>';
