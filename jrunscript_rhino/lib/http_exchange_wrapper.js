9ea88de35bdaceb8a02fb19993b93ba1537e4cc1
sonota88 <yosiot8753@gmail.com>
2015-02-14 21:13:42 +0900

    add HttpExchangeWrapper.prototype.writeFileResponse

--- jrunscript_rhino/lib/http_exchange_wrapper.js
+++ jrunscript_rhino/lib/http_exchange_wrapper.js
@@ -10,6 +10,15 @@ var HttpExchangeWrapper = (function(){
     return new java.lang.String(jsString);
   };
 
+  function withFileInputStream(file, proc){
+    var fis = new FileInputStream(file);
+    try{
+      proc(fis);
+    } finally {
+      if(fis){ fis.close(); }
+    }
+  }
+
   function withOutputStreamWriter(fos, encoding, fn){
     var osw;
     try {
@@ -28,6 +37,31 @@ var HttpExchangeWrapper = (function(){
     } finally {
       if(bw){ bw.close(); }
     }
+  }
+
+  var contentTypes = {
+    "jpg": "image/jpeg"
+    , "jpeg": "image/jpeg"
+    , "png": "image/png"
+    , "svg": "image/svg+xml"
+    , "gif": "image/gif"
+    , "css": "text/css"
+    , "js": "text/javascript"
+    , "json": "application/json"
+  };
+
+  function getContentType(path){
+    if(path.match(/(.+)\.(.+)$/)){
+      var ext = RegExp.$2.toLowerCase();
+      return contentTypes[ext];
+    }else{
+      return null;
+    }
+  }
+
+  function createByteArray(size){
+    return java.lang.reflect.Array.newInstance(
+      java.lang.Byte.TYPE, size);
   }
 
 
@@ -66,6 +100,33 @@ var HttpExchangeWrapper = (function(){
     });
 
     os.close();
+  };
+
+  __.writeFileResponse = function(path){
+    var resOS = this._he.getResponseBody();
+
+    var file = new File(path);
+
+    var contentType = getContentType(path);
+    // putskv("contentType", contentType);
+
+    if(contentType){
+      this._he.getResponseHeaders().add(
+        "Content-Type", contentType);
+    }
+
+    this._he.sendResponseHeaders(
+      200, file.length());
+
+    withFileInputStream(file, function(fis){
+      var buf = createByteArray(1024);
+      var len;
+      while((len = fis.read(buf)) >= 0){
+        resOS.write(buf, 0, len);
+      }
+    });
+
+    resOS.close();
   };
 
   return HttpExchangeWrapper;
