3773cdb1c8061572febb11b7814841f4ba957d52
sonota88 <yosiot8753@gmail.com>
2015-04-05 18:51:58 +0900

    fix indent

--- jrunscript_rhino/test/byte_array_utils.js
+++ jrunscript_rhino/test/byte_array_utils.js
@@ -5,20 +5,20 @@ function b2s(bytes){
   return "" + new java.lang.String(bytes);
 }
 
-  var orig = new java.lang.String("abcde").getBytes();
-  assert(b2s(orig), "abcde");
+var orig = new java.lang.String("abcde").getBytes();
+assert(b2s(orig), "abcde");
 
-  (function(){
-    var act = ByteArrayUtils.substr(orig, 0, 2);
-    assert(b2s(act), "ab");
-  })();
+(function(){
+  var act = ByteArrayUtils.substr(orig, 0, 2);
+  assert(b2s(act), "ab");
+})();
 
-  (function(){
-    var act = ByteArrayUtils.substr(orig, 3, 5);
-    assert(b2s(act), "de");
-  })();
+(function(){
+  var act = ByteArrayUtils.substr(orig, 3, 5);
+  assert(b2s(act), "de");
+})();
 
-  (function(){
-    var act = ByteArrayUtils.substr(orig, 2);
-    assert(b2s(act), "cde");
-  })();
+(function(){
+  var act = ByteArrayUtils.substr(orig, 2);
+  assert(b2s(act), "cde");
+})();
--- jrunscript_rhino/test/optparse.js
+++ jrunscript_rhino/test/optparse.js
@@ -1,44 +1,44 @@
 var Optparse = require("lib/optparse");
 
-  (function(){
-    puts("no options");
-    var args = ["a", "b"];
-    var opts = Optparse.parse(args);
-    assert(opts.shift(), "a");
-    assert(opts.shift(), "b");
-  })();
+(function(){
+  puts("no options");
+  var args = ["a", "b"];
+  var opts = Optparse.parse(args);
+  assert(opts.shift(), "a");
+  assert(opts.shift(), "b");
+})();
 
-  (function(){
-    puts("not required");
-    var args = ["a", "-b"];
-    var opts = Optparse.parse(args, [
-      ["-b"]
-    ]);
-    assert(opts.shift(), "a");
-    assert(opts.has("-b"), true);
-    assert(opts.has("-a"), false);
-  })();
+(function(){
+  puts("not required");
+  var args = ["a", "-b"];
+  var opts = Optparse.parse(args, [
+    ["-b"]
+  ]);
+  assert(opts.shift(), "a");
+  assert(opts.has("-b"), true);
+  assert(opts.has("-a"), false);
+})();
 
-  (function(){
-    puts("required");
-    var args = ["-a", "123"];
+(function(){
+  puts("required");
+  var args = ["-a", "123"];
+  var opts = Optparse.parse(args, [
+    ["-a", true]
+  ]);
+  assert(opts.rest.length, 0);
+  assert(opts.has("-a"), true);
+  assert(opts.valueOf("-a"), "123");
+})();
+
+(function(){
+  puts("required but no value");
+  var args = ["-a"];
+  try{
     var opts = Optparse.parse(args, [
       ["-a", true]
     ]);
-    assert(opts.rest.length, 0);
-    assert(opts.has("-a"), true);
-    assert(opts.valueOf("-a"), "123");
-  })();
-
-  (function(){
-    puts("required but no value");
-    var args = ["-a"];
-    try{
-      var opts = Optparse.parse(args, [
-        ["-a", true]
-      ]);
-      fail("must throw 1");
-    } catch (x) {
-      // OK
-    }
-  })();
+    fail("must throw 1");
+  } catch (x) {
+    // OK
+  }
+})();
