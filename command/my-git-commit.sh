a6e98b9c4962cd028b6d34ab0cc132a617268ec6
sonota88 <yosiot8753@gmail.com>
2020-08-16 11:35:15 +0900

    __wip 2020-08-16 11:35:15 (command/my-git-commit.sh)

--- command/my-git-commit.sh
+++ command/my-git-commit.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
 
+set -o errexit
+
+changed_files() {
+  local all_files="$(
+    git status --porcelain --untracked-file=no \
+      | egrep -v '^ ' \
+      | awk '{ print $NF }'
+  )"
+
+  local size=$(echo "$all_files" | wc -l)
+
+  local size_limit=4
+
+  if [ $size -gt $size_limit ]; then
+    files="$(
+      echo "$all_files" | head -${size_limit}
+      printf "..."
+    )"
+  else
+    files="$all_files"
+  fi
+
+  echo "$files" \
+    | awk '{ printf "%s, ", $0 }' \
+    | awk '{ sub(/, $/, ""); print }'
+}
+
+# --------------------------------
+
 tmpfile=/tmp/my_git_commit_template.txt
 
 {
-  echo "__wip $(date "+%F %T")"
+  echo "__wip $(date "+%F %T") ($(changed_files))"
   printf "\n"
 } > $tmpfile
 
