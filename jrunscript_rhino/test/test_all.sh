0ff302471b440fcab150f11a84cc42d7bbd791aa
sonota88 <yosiot8753@gmail.com>
2022-12-03 13:59:56 +0900

    rm files

--- jrunscript_rhino/test/test_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# export JJS_STDLIB_DIR=/foo/bar/stdlib
-
-jrunscript test.js test_byte_array_utils
-jrunscript test.js test_optparse
-jrunscript test.js test_base64
--- soymilk/README.md
+++
@@ -1,3 +0,0 @@
-moved to
-https://github.com/sonota/soymilk
-
