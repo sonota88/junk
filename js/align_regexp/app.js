5a1289733b4068ac871d8dc68ceb041918c0e3c9
sonota88 <yosiot8753@gmail.com>
2022-12-03 13:54:54 +0900

    js/align_regexp

--- js/align_regexp/app.js
+++ js/align_regexp/app.js
@@ -1,69 +1,214 @@
-// TODO 全角文字の幅を考慮
-
 function puts(...args) {
-  console.log(...args);
+  // console.log(...args);
 }
 
-function _el(sel) {
+function getEl(sel) {
   return document.querySelector(sel);
 }
 
-function _els(sel) {
-  return document.querySelectorAll(sel);
+function handleEv(el, ev, fn) {
+  let els;
+  if (typeof el === "string") {
+    els = document.querySelectorAll(el);
+  } else {
+    els = [el];
+  }
+  els.forEach(el => el.addEventListener(ev, fn));
 }
 
-function addev(el, ev, fn) {
-  el.addEventListener(ev, fn);
+function debounce(fn, msec) {
+  const self = debounce;
+
+  if (self.map[fn] != null) {
+    clearTimeout(self.map[fn]);
+  }
+
+  self.map[fn] = setTimeout(
+    () => {
+      fn();
+      self.map[fn] = null;
+    },
+    msec
+  );
+}
+debounce.map = {};
+
+// --------------------------------
+
+function getPattern() {
+  return getEl("#pattern").value;
 }
 
-function refresh() {
-  const input = _el("#input").value;
-  const pattern = _el("#pattern").value;
+function getIndex() {
+  return Math.trunc(getEl("#index").value);
+}
 
+function getInput() {
+  return getEl("#input").value;
+}
+
+function setInput(text) {
+  getEl("#input").value = text;
+}
+
+function getPreview() {
+  return getEl("#preview").value;
+}
+
+function setPreview(text) {
+  getEl("#preview").value = text;
+}
+
+// --------------------------------
+
+function isHankaku(c) {
+  const cc = c.charCodeAt(0);
+  return (
+    (32 <= cc && cc <= 126) // (SPC) ... '~'
+    || (65377 <= cc && cc <= 65439) // '｡' ... 'ﾟ'
+  );
+}
+
+function charWidth(c) {
+  return isHankaku(c) ? 1 : 2;
+}
+
+function lengthInHankaku(s) {
+  let len = 0;
+  for (let i = 0; i < s.length; i++) {
+    len += charWidth(s.charAt(i));
+  }
+  return len;
+}
+
+/*
+ * left sep right
+ *     ^m.index == left length
+ *      ^^^m[0]
+ */
+function matchLine(line, re, index) {
+  let rest = line;
+  let found = false;
+  let pos = 0;
+
+  let i = -1;
+  while (true) {
+    i++;
+    if (1000 < i) {
+      throw new Error("too many iteration");
+    }
+
+    const m = rest.match(re);
+    if (m == null) {
+      break;
+    }
+
+    pos += m.index; // add left
+
+    if (index <= i) {
+      found = true;
+      break;
+    }
+
+    pos += m[0].length; // add separator
+    rest = rest.substring(m.index + m[0].length);
+  }
+
+  return found ? pos : null;
+}
+
+function getAlignPos(pairs) {
+  const headLenList =
+    pairs
+      .map(pair => pair[0])
+      .map(lengthInHankaku);
+
+  return Math.max(...headLenList);
+}
+
+function toPair(line, re, index) {
+  const pos = matchLine(line, re, index);
+  if (pos == null) {
+    return [line, ""];
+  } else {
+    const head = line.substring(0, pos);
+    const rest = line.substring(pos);
+    return [head, rest];
+  }
+}
+
+function alignRegexp(input, re, index) {
   const lines = input.split("\n");
+  const pairs = lines.map(line => toPair(line, re, index));
+  const alignPos = getAlignPos(pairs);
 
-  // 桁の位置を決める
-  const llens = lines.map(line => {
-    const i = line.indexOf(pattern);
-    if (0 <= i) {
-      return i;
-    } else {
-      return 0;
-    }
-  });
-  puts(32, Math.max(...llens));
+  const newLines =
+    pairs.map(pair => {
+      const [head, rest] = pair;
+      if (rest === "") {
+        return head;
+      } else {
+        const numSpaces = alignPos - lengthInHankaku(head);
+        return head + " ".repeat(numSpaces) + " " + rest;
+      }
+    });
 
-  // 揃える位置
-  const i = Math.max(...llens);
-  puts(32, llens, i);
+  return newLines.join("\n");
+}
 
-  const lines2 = lines.map(line => {
-    const i2 = line.indexOf(pattern);
-    const left = line.substring(0, i2);
-    const right = line.substring(i2);
-    const num_spcs = i - left.length;
-    puts(left, right, num_spcs);
+function _refreshPreview() {
+  const pattern = getPattern();
+  if (pattern === "") {
+    setPreview("");
+    return;
+  }
+  const re = new RegExp(pattern);
 
-    let left2 = left;
-    for (let i3 = 0; i3 < num_spcs; i3++) {
-      left2 = left2 + " ";
-    }
+  const input = getInput();
+  const index = getIndex();
 
-    return left2 + right;
-  });
+  const aligned = alignRegexp(input, re, index);
 
-  const output = lines2.join("\n");
+  setPreview(aligned);
+}
 
-  _el("#output").value = output;
+function refreshPreview() {
+  try {
+    _refreshPreview();
+  } catch(e) {
+    setPreview(e.message);
+    throw e;
+  }
+}
+
+function doApply() {
+  const text = getPreview();
+  setInput(text);
+}
+
+function oninput_text() {
+  debounce(refreshPreview, 100);
+}
+
+function onclick_apply() {
+  doApply();
+}
+
+function onkeydown(ev) {
+  if (ev.ctrlKey && ev.key === "Enter") {
+    doApply();
+  }
 }
 
 function init() {
-  addev(_el("#pattern"), "input", ()=>{
-    refresh();
-  });
-  addev(_el("#input"), "input", ()=>{
-    refresh();
-  });
+  refreshPreview();
+
+  ["#pattern", "#input", "#index"]
+    .forEach(sel =>
+      handleEv(sel, "input", oninput_text)
+    );
+  handleEv("#btn_apply", "click", onclick_apply)
+  handleEv(window, "keydown", (ev) => onkeydown(ev));
 }
 
 window.addEventListener("pageshow", init);
